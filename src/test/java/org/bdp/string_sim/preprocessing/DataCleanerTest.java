bae5c1d24075160534ea6c2eb3e18733d883705c
Lunev1 <fl.zeidler@gmail.com>
2016-06-13 18:13:04 +0200

    #4 debug fixed

--- src/test/java/org/bdp/string_sim/preprocessing/DataCleanerTest.java
+++ src/test/java/org/bdp/string_sim/preprocessing/DataCleanerTest.java
@@ -26,21 +26,21 @@ public class DataCleanerTest extends TestCase {
         return environment.readCsvFile(testData.toString())
                 .includeFields("11")
                 .fieldDelimiter(";")
-                .ignoreInvalidLines()
+                //.ignoreInvalidLines()
                 .types(Integer.class,String.class);
     }
     
     public void testDataCleaner() throws Exception {
     	DataSet<Tuple2<Integer, String>> TestDataSet = this.getDataCleanerTestDataFromCsv();
     	DataSet<Tuple2<Integer, String>> CleanedTestDataSet = TestDataSet.map(new DataCleaner());
+    	assertTrue(CleanedTestDataSet.count() == 9);
     	
     	//CleanedTestDataSet.print();
-    	
     	List<Tuple2<Integer, String>> collectList = CleanedTestDataSet.collect();
         for(Tuple2<Integer, String> tuple : collectList)
         {	
-        	
             assertEquals((String)"dresden",(String)tuple.getField(1));
         }
+        
     }
 }
