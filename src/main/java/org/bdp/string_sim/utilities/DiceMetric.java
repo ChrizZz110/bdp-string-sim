444b8e5fb74c56a3c407778f6ba4334445dc9e5b
Lunev1 <flurx@web.de>
2016-07-25 04:05:59 +0200

    #added test class

--- src/main/java/org/bdp/string_sim/utilities/DiceMetric.java
+++ src/main/java/org/bdp/string_sim/utilities/DiceMetric.java
@@ -1,7 +1,14 @@
 package org.bdp.string_sim.utilities;
 
 public class DiceMetric {
-	
+	/**
+     * This Function calculates the dicemetric based on given numbers of token for both strings and the number of matches between them
+     * @param numberTokensA number of tokens for string A
+     * @param numberTokensB number of tokens for string B
+     * @param match number of matching tokens between string A and string B 
+     * @return calculated metric of type float
+     * @throws Exception
+     */
 	public static float calculate(int numberTokensA, int numberTokensB, int match) {
 		
 		float metric = (2*(float)match) / (numberTokensA+numberTokensB);
