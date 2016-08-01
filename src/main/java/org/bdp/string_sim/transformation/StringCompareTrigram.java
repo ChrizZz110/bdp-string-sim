141017b37f11a9e6025d172d851637a4faa1dcad
Lunev1 <flurx@web.de>
2016-08-01 09:37:45 +0200

    #8 function fixed, test added

--- src/main/java/org/bdp/string_sim/transformation/StringCompareTrigram.java
+++ src/main/java/org/bdp/string_sim/transformation/StringCompareTrigram.java
@@ -16,31 +16,53 @@ public class StringCompareTrigram implements FlatMapFunction<Tuple4<Integer,Stri
 
     public StringCompareTrigram() {
     }
-
-    public StringCompareTrigram(float collectOnlyMatchingStrings) {
-        this.thresholdMatch = collectOnlyMatchingStrings;
+    
+    /**
+     * This constructor creates an StringCompareTrigram instance.
+     * @param thresholdMatch only collects matches equal or above this threshold. threshold must be between 0.0 and 1.0.
+     */
+    public StringCompareTrigram(float thresholdMatch) {
+    	if (thresholdMatch >= 0.0 && thresholdMatch <= 1.0) {
+    		this.thresholdMatch = thresholdMatch;
+    	}
     }
-
+    
+    /**
+     * This flatMap function compares strings in field 1 and 3 of the input tuple4 by tokenization its strings and compare it against
+     * each other. According to matching tokens a diceMetric is calculated and added in a ResultTuple5. Matches will be only be saved if they 
+     * have a sufficient diceMetric, according to propterty thresholdMatch.
+     * @param input the input tuple4:
+     *              Field 0: the id of label A
+     *              Field 1: the label A
+     *              Field 2: the id of label B
+     *              Field 3: the label B
+     * @param collector the collector which collects all result tuple of type ResultTuple5
+     * @throws Exception
+     */
     @Override
     public void flatMap(Tuple4<Integer, String, Integer, String> input, Collector<ResultTuple5> collector) throws Exception {
     	
     	Tokenizer tokenizer = new Tokenizer();
     	ArrayList <String> stringListA = tokenizer.tokenize(input.getField(1));
     	ArrayList <String> stringListB = tokenizer.tokenize(input.getField(3));
-    	
+    	Integer sizeListA = stringListA.size();
+    	Integer sizeListB = stringListB.size();
     	Integer match = 0;
+    	
     	for (String a : stringListA){
     		for (String b : stringListB){
     			if (a.equals(b)){
     				match++;
+    				// only one match per token due to integrity of diceMetric
+    				stringListB.remove(b);
     				break;
     			}
     		}
     	}
     	
-    	float diceMetric = DiceMetric.calculate(stringListA.size(), stringListB.size(), match);
+    	float diceMetric = DiceMetric.calculate(sizeListA, sizeListB, match);
     	
-    	//if (diceMetric >= thresholdMatch) 
+    	if (diceMetric >= thresholdMatch) 
     		collector.collect(new ResultTuple5(
 								    			input.getField(0),
 								    			input.getField(1),
